952b3be028e56253d4c99dc37e195faff5ec60d5
Carlos Santana <carlos@milkzoft.com>
2011-07-27 18:11:57 -0500

    HMVC

---
+++ applications/default/controllers/controller.otro.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Access from index.php:
+ */
+if(!defined("_access")) {
+	die("Error: You don't have permission to access here...");
+}
+
+class Otro_Controller extends ZP_Load {
+	
+	public function __construct() {
+	
+	}
+	
+	public function index() {		
+		print __("Hi, I'm the otro controllador of default application");
+	}
+	
+	public function prueba2($p1, $p2, $p3) {
+		print $p3 . $p2 . $p2;	
+	}
+}
