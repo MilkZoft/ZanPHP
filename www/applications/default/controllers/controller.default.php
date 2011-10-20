177503129c9212596a5b2c1a6eefc814c02c981c
Carlos Santana <carlos@milkzoft.com>
2011-10-20 14:26:34 -0500

    Default application

--- www/applications/default/controllers/controller.default.php
+++ www/applications/default/controllers/controller.default.php
@@ -10,25 +10,12 @@ class Default_Controller extends ZP_Controller {
 	
 	public function __construct() {
 		$this->app("default");
-		
-		$this->Default_Model = $this->model("Default_Model");
-		
+				
 		$this->Templates = $this->core("Templates");
 	}
 	
 	public function index() {		
-		if(isLang() and segment(2) === "otrometodo" and segment(3) > 0 and segment(4) > 0) {
-			$this->suma(segment(3), segment(4));
-		} elseif(segment(1) === "otrometodo" and segment(2) > 0 and segment(3) > 0)
-			$this->suma(segment(2), segment(3));
-	}
-		
-	private function suma($a, $b) {
-		print ($a + $b);
-	}
-	
-	public function imprimir($texto) {
-		print $texto;
+		print "Welcome to ZanPHP";
 	}
 
 }
