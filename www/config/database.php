2bc631e57bc8caabfaa4a3733ad5ae5cf26b1424
Carlos Santana <carlos@milkzoft.com>
2012-03-22 00:20:33 -0600

    Renombramiento masivo

---
+++ www/config/database.php
@@ -0,0 +1,35 @@
+<?php
+/* ex: set tabstop=2 noexpandtab: */
+/**
+ * Access from index.php:
+ */
+if(!defined("_access")) {
+	die("Error: You don't have permission to access here...");
+}
+
+/**
+ *	SQL Databases
+ */
+$ZP["db"]["dbController"] = "mysqli";
+$ZP["db"]["dbHost"] 	  = "localhost";
+$ZP["db"]["dbUser"] 	  = "root";
+$ZP["db"]["dbPwd"] 	      = "";
+$ZP["db"]["dbName"] 	  = "";
+$ZP["db"]["dbPort"] 	  = 3306;
+$ZP["db"]["dbPfx"] 	      = "zan_";
+$ZP["db"]["dbSocket"]     = NULL;
+
+/**
+ *	SQLite Databases
+ */
+$ZP["db"]["dbFilename"] = "mydatabase.db";
+$ZP["db"]["dbMode"]	    = 0666;
+	
+/**
+ *	NoSQL Databases
+ */
+$ZP["db"]["dbNoSQLHost"]  	 = "localhost";
+$ZP["db"]["dbNoSQLPort"] 	 = 27017;
+$ZP["db"]["dbNoSQLUser"] 	 = ""; 
+$ZP["db"]["dbNoSQLPwd"]  	 = "";
+$ZP["db"]["dbNoSQLDatabase"] = "";
